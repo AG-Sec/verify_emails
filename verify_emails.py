2d43c435ccde5ed2673a5870f9ba2196352f4aeb
AG-Sec <89414244+AG-Sec@users.noreply.github.com>
2023-09-15 17:22:09 -0500

    Update verify_emails.py

--- verify_emails.py
+++ verify_emails.py
@@ -21,7 +21,7 @@ if upload is not None:
         v = validate(a)
         res.append(v)
     st.table(res)
-    df_csv = normalized_payload.to_csv().encode('utf-8')
+    df_csv = res.to_csv().encode('utf-8')
     st.download_button(label="Download Results!",
                                     data=df_csv,
                                     file_name="results.csv",
